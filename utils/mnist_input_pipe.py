4a291453b1d81fd67c17a61175648ff1df8a6626
JMEusebio <jeusebio@asu.edu>
2017-05-09 16:59:18 -0700

    Adding utilities to load mnist/svhn data into pipeline

---
+++ utils/mnist_input_pipe.py
@@ -0,0 +1,93 @@
+###############################################################################
+##############################################################################
+# Author:       Imanol Schlag (more info on ischlag.github.io)
+# Description:  MNIST input pipeline
+# Date:         11.2016
+#
+# Note: Only uses one queue,
+
+""" Usage:
+import tensorflow as tf
+with tf.device('/cpu:0'):
+  from datasets.mnist import mnist_data
+  data = mnist_data(53)
+  image_batch_tensor, target_batch_tensor = data.build_train_data_tensor()
+sess = tf.Session()
+init_op = tf.initialize_all_variables()
+sess.run(init_op)
+coord = tf.train.Coordinator()
+threads = tf.train.start_queue_runners(coord=coord, sess=sess)
+image_batch, target_batch = sess.run([image_batch_tensor, target_batch_tensor])
+print(image_batch.shape)
+print(target_batch.shape)
+"""
+
+import tensorflow as tf
+
+from tensorflow.python.framework import ops
+from tensorflow.examples.tutorials.mnist import input_data
+
+class mnist_data:
+  """
+  Downloads the MNIST dataset and creates an input pipeline ready to be fed into a model.
+  - Reshapes flat images into 28 x 28
+  - converts [0 1] to [-1 1]
+  - shuffles the input
+  - builds batches
+  """
+  NUM_THREADS = 8
+  NUMBER_OF_CLASSES = 10
+  IMAGE_WIDTH = 28
+  IMAGE_HEIGHT = 28
+  NUM_OF_CHANNELS = 1
+
+  def __init__(self, batch_size):
+    """ Downloads the mnist data if necessary. """
+    print("Loading MNIST data")
+    self.batch_size = batch_size
+    self.mnist = input_data.read_data_sets('data/MNIST', one_hot=True)
+
+    self.TRAIN_SET_SIZE = self.mnist.train.images.shape[0]
+    self.TEST_SET_SIZE = self.mnist.test.images.shape[0]
+    self.VALIDATION_SET_SIZE = self.mnist.validation.images.shape[0]
+
+  def build_train_data_tensor(self, shuffle=False, augmentation=False):
+    return self.__build_generic_data_tensor(self.mnist.train.images,
+                                            self.mnist.train.labels,
+                                            shuffle,
+                                            augmentation)
+
+  def build_test_data_tensor(self, shuffle, augmentation=False):
+    return self.__build_generic_data_tensor(self.mnist.test.images,
+                                            self.mnist.test.labels,
+                                            shuffle,
+                                            augmentation)
+
+  def build_validation_data_tensor(self, shuffle, augmentation=False):
+    return self.__build_generic_data_tensor(self.mnist.validation.images,
+                                            self.mnist.validation.labels,
+                                            shuffle,
+                                            augmentation)
+
+  def __build_generic_data_tensor(self, raw_images, raw_targets, shuffle, augmentation):
+    """ Creates the input pipeline and performs some preprocessing. """
+
+    images = ops.convert_to_tensor(raw_images)
+    targets = ops.convert_to_tensor(raw_targets)
+
+    set_size = raw_images.shape[0]
+
+    images = tf.reshape(images, [set_size, 28, 28, 1])
+    image, label = tf.train.slice_input_producer([images, targets], shuffle=shuffle)
+
+    # Data Augmentation
+    if augmentation:
+      image = tf.image.resize_image_with_crop_or_pad(image, self.IMAGE_HEIGHT+4, self.IMAGE_WIDTH+4)
+      image = tf.random_crop(image, [self.IMAGE_HEIGHT, self.IMAGE_WIDTH, self.NUM_OF_CHANNELS])
+      image = tf.image.random_flip_left_right(image)
+
+    image = tf.image.per_image_standardization(image)
+
+    images_batch, labels_batch = tf.train.batch([image, label], batch_size=self.batch_size, num_threads=self.NUM_THREADS)
+
+    return images_batch, labels_batch
---
+++ utils/svhn_data.py
@@ -0,0 +1,66 @@
+###############################################################################
+# Author:       Imanol Schlag (more info on ischlag.github.io)
+# Description:  Functions to download and load SVHN into memory.
+# Date:         10.11.2016
+#
+#
+
+from utils import download
+import scipy.io
+
+data_path = "data/SVHN/"
+
+data_url = 'http://ufldl.stanford.edu/housenumbers/'
+train_data = 'train_32x32.mat'
+test_data = 'test_32x32.mat'
+extra_data = 'extra_32x32.mat'
+
+num_classes = 10
+
+def download_data():
+  """Download the SVHN data if it doesn't exist yet."""
+
+  download.maybe_download(url=data_url + train_data, download_dir=data_path)
+  download.maybe_download(url=data_url + test_data, download_dir=data_path)
+  download.maybe_download(url=data_url + extra_data, download_dir=data_path)
+
+def load_training_data():
+  """
+  Load all the training-data for the SVHN data-set.
+  Returns the images, class-numbers and one-hot encoded class-labels.
+  """
+
+  train_data = scipy.io.loadmat(data_path + 'train_32x32.mat', variable_names='X').get('X')
+  train_labels = scipy.io.loadmat(data_path + 'train_32x32.mat', variable_names='y').get('y')
+
+  images = train_data.transpose((3,0,1,2)) / 255.0
+  cls = train_labels[:, 0]
+  cls[cls == 10] = 0
+
+  return images, cls, download.one_hot_encoded(class_numbers=cls, num_classes=num_classes)
+
+def load_test_data():
+  """
+  Load all the test-data for the SVHN data-set.
+  Returns the images, class-numbers and one-hot encoded class-labels.
+  """
+
+  test_data = scipy.io.loadmat(data_path + 'test_32x32.mat', variable_names='X').get('X')
+  test_labels = scipy.io.loadmat(data_path + 'test_32x32.mat', variable_names='y').get('y')
+
+  images = test_data.transpose((3, 0, 1, 2)) / 255.0
+  cls = test_labels[:, 0]
+  cls[cls == 10] = 0
+
+  return images, cls, download.one_hot_encoded(class_numbers=cls, num_classes=num_classes)
+
+
+def load_extra_data():
+  extra_data = scipy.io.loadmat(data_path + 'extra_32x32.mat', variable_names='X').get('X')
+  extra_labels = scipy.io.loadmat(data_path + 'extra_32x32.mat', variable_names='y').get('y')
+
+  images = extra_data.transpose((3,0,1,2)) / 255.0
+  cls = extra_labels[:, 0]
+  cls[cls == 10] = 0
+
+  return images, cls, download.one_hot_encoded(class_numbers=cls, num_classes=num_classes)
